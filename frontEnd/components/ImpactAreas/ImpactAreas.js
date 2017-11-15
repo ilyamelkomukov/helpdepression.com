477cb5cd5a748f24d7f3932cef47e5c15ff68dfe
ilyamelkomukov <schvartzeeenbiggy@gmail.com>
2017-11-15 18:05:42 +0300

    Some fix for showing the clarifications of impact areas

--- frontEnd/components/ImpactAreas/ImpactAreas.js
+++ frontEnd/components/ImpactAreas/ImpactAreas.js
@@ -24,23 +24,34 @@ let ImpactAreas = {
       .hover(
         function(e) {
 
+
           var target = e.target;
 
           $(target).addClass('_segment_hover');
 
+          if (_impactClarifications.is(function () {
+            return $(this).data('showing') == 'in-process'
+          })) {
+            return
+          }
+
           _impactClarifications
             .filter(function(index, elem) {
 
-              return target.getAttribute('id').slice(-1) ==
-                $(this)
-                  .data('segment-number');
+              return target.getAttribute('id').endsWith($(this).data('segment-number'));
             })
+              .data('showing', 'in-process')
               .css({
                 display: 'block',
               })
               .animate({
                 opacity: 1
-              }, 300);
+              }, 300, function () {
+                $(this)
+                  .data('showing', 'static');
+
+                  console.log();
+              });
         },
         function(e) {
 
